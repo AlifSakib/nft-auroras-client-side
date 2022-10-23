3f9937309ca2868a6396f3aaa5e44cf832aba8a0
AlifSakib <alifsakib@gmail.com>
2022-10-24 02:45:18 +0600

    collectiong data in controlled way

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,16 @@
+import { getAuth } from "firebase/auth";
+import React, { createContext } from "react";
+import app from "../firebase/firebase.config";
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const value = {};
+  return (
+    <div>
+      <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    </div>
+  );
+};
+
+export default AuthProvider;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqs5uLBdfIeQElaSVGO-adf_DprUu6WxU",
+  authDomain: "aurora-9b6cc.firebaseapp.com",
+  projectId: "aurora-9b6cc",
+  storageBucket: "aurora-9b6cc.appspot.com",
+  messagingSenderId: "802216440529",
+  appId: "1:802216440529:web:d124c8eaf0864c2172cbf0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
